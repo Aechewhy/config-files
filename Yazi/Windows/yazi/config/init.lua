bc083222b8913bfbd592ddafdba41bfb2c4b303c
Aechewhy <harryhuy2472004@gmail.com>
2025-04-30 20:07:04 +0700

    Yazi image preview and AHK scripts

--- Yazi/Windows/yazi/config/init.lua
+++ Yazi/Windows/yazi/config/init.lua
@@ -1,13 +1,47 @@
--- ~/.config/yazi/init.lua
---require("arrow"):setup()
+function Linemode:btime()
+	local time = math.floor(self._file.cha.btime or 0)
+	if time == 0 then
+		return ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		return os.date("%m/%d/%y %H:%M", time)
+	else
+		return os.date("%m/%d/%y %H:%M", time)
+	end
+end
+
+function Linemode:mtime()
+	local time = math.floor(self._file.cha.mtime or 0)
+	if time == 0 then
+		return ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		return os.date("%m/%d/%y %H:%M", time)
+	else
+		return os.date("%m/%d/%y %H:%M", time)
+	end
+end
 require("relative-motions"):setup({ show_numbers="relative", show_motion = true, enter_mode ="first" })
 
 require("full-border"):setup()
 
 require("git"):setup()
 
-require("folder-rules"):setup()
+require("mime-ext"):setup {
+	-- Expand the existing filename database (lowercase), for example:
+	with_files = {
+		makefile = "text/makefile",
+		-- ...
+	},
 
+	-- -- Expand the existing extension database (lowercase), for example:
+	with_exts = {
+		mk = "text/makefile",
+		-- ...
+	},
+
+	-- -- If the mime-type is not in both filename and extension databases,
+	-- -- then fallback to Yazi's preset `mime` plugin, which uses `file(1)`
+	fallback_file1 = false,
+}
 require("copy-file-contents"):setup({
 	clipboard_cmd = "clip",
 	append_char = "\n",
@@ -25,10 +59,10 @@ require("bunny"):setup({
 
 require("pref-by-location"):setup({
 -- Disable this plugin completely.
--- disabled = false -- true|false (Optional)
+	disabled = false,-- true|false (Optional)
 
 -- Hide "enable" and "disable" notifications.
--- no_notify = false -- true|false (Optional)
+	no_notify = false, -- true|false (Optional)
 
 -- You can backup/restore this file. But don't use same file in the different OS.
 -- save_path =  -- full path to save file (Optional)
@@ -97,16 +131,16 @@ require("searchjump"):setup({
 	search_patterns = ({"hell[dk]d","%d+.1080p","第%d+集","第%d+话","%.E%d+","S%d+E%d+",})
 })
 
-require("eza-preview"):setup({
-    -- -- Determines the directory depth level to tree preview (default: 3)
-    -- level = 3,
+-- require("eza-preview"):setup({
+--     -- -- Determines the directory depth level to tree preview (default: 3)
+--     -- level = 3,
 
-    -- -- Whether to follow symlinks when previewing directories (default: false)
-    -- follow_symlinks = false
+--     -- -- Whether to follow symlinks when previewing directories (default: false)
+--     -- follow_symlinks = false
 
-    -- -- Whether to show target file info instead of symlink info (default: false)
-    -- dereference = false
-})
+--     -- -- Whether to show target file info instead of symlink info (default: false)
+--     -- dereference = false
+-- })
 
 
 require("yatline"):setup({
@@ -202,55 +236,4 @@ require("yatline"):setup({
 		}
 	},
 })
-})
-
-require("omp"):setup({config= "C:/Users/LENOVO LEGION/OneDrive - Hanoi University of Science and Technology/Documents/oh-my-posh-themes/gruvbox.omp.json"})
---Simple tag
-require("simple-tag"):setup({
--- UI display mode (icon, text, hidden)
-ui_mode = "icon", -- (Optional)
-
--- Disable tag key hints (popup in bottom right corner)
-hints_disabled = false, -- (Optional)
-
--- linemode order: adjusts icon/text position. Fo example, if you want icon to be on the mose left of linemode then set linemode_order larger than 1000.
--- More info: https://github.com/sxyazi/yazi/blob/077faacc9a84bb5a06c5a8185a71405b0cb3dc8a/yazi-plugin/preset/components/linemode.lua#L4-L5
-linemode_order = 500, -- (Optional)
-
--- You can backup/restore this folder. But don't use backed up folder in the different OS.
--- save_path =  -- full path to save tags folder (Optional)
---       - Linux/MacOS: os.getenv("HOME") .. "/.config/yazi/tags"
---       - Windows: os.getenv("APPDATA") .. "\\yazi\\config\\tags"
-
--- Set tag colors
-colors = { -- (Optional)
--- Set this same value with `theme.toml` > [manager] > hovered > reversed
--- Default theme use "reversed = true".
--- More info: https://github.com/sxyazi/yazi/blob/077faacc9a84bb5a06c5a8185a71405b0cb3dc8a/yazi-config/preset/theme-dark.toml#L25
-reversed = true, -- (Optional)
-
--- More colors: https://yazi-rs.github.io/docs/configuration/theme#types.color
--- format: [tag key] = "color"
-["*"] = "#bf68d9", -- (Optional)
-["$"] = "green",
-["!"] = "#cc9057",
-["1"] = "cyan",
-["p"] = "red",
-},
-
--- Set tag icons. Only show when ui_mode = "icon".
--- Any text or nerdfont icons should work
--- Default icon from mactag.yazi: ●; , , 󱈤
--- More icon from nerd fonts: https://www.nerdfonts.com/cheat-sheet
-icons = { -- (Optional)
--- default icon
-default = "󰚋",
-
--- format: [tag key] = "tag icon"
-["*"] = "*",
-["$"] = "",
-["!"] = "",
-["p"] = "",
-},
-
 })
